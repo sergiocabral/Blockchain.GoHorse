ced5d0d94e1e9211b230c8e7f3b37ef10e66ec63
Sergio Cabral <git@sergiocabral.com>
2021-06-01 19:17:58 -0300

    Extendido classe String com prototype.

---
+++ src/Prototype/String.ts
@@ -0,0 +1,40 @@
+import {Text} from "../Helper/Text";
+
+declare global {
+    /**
+     * Interface para extender propriedades de String.
+     */
+    interface String {
+        /**
+         * Substitui todas as ocorrências de uma string.
+         * @param search String procurada.
+         * @param replacement String de substituição.
+         */
+        replaceAll(search: string, replacement: string): string;
+
+        /**
+         * Escapa uma string para ser usada como literal numa expressão regular.
+         */
+        escapeRegExp(): string;
+
+        /**
+         * Substitui variáveis na string por seus respectivos valores.
+         * @param values Opcional. Conjunto de valores para substituição na string.
+         */
+        querystring(values: any): string;
+    }
+}
+
+String.prototype.replaceAll = function(search: string, replacement: string): string {
+    return Text.replaceAll(String(this), search, replacement);
+};
+
+String.prototype.escapeRegExp = function(): string {
+    return Text.escapeRegExp(String(this));
+};
+
+String.prototype.querystring = function(values: any): string {
+    return Text.querystring(String(this), values);
+};
+
+export { }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {MainApp} from './Core/MainApp';
 import environment from './env.json';
+import './Prototype/String';
 
 new MainApp(environment).run();
