beabd52c2bba5201b56aaeaf75a93b776447a095
Sergio Cabral <git@sergiocabral.com>
2021-09-29 10:28:30 -0300

    code: BasicProtocol trafega mensagens em base64.

--- ts/WebSocket/Protocol/BasicProtocol.ts
+++ ts/WebSocket/Protocol/BasicProtocol.ts
@@ -1,4 +1,4 @@
-import { Logger, LogLevel } from "@sergiocabral/helper";
+import { InvalidDataError, Logger, LogLevel } from "@sergiocabral/helper";
 
 import { WebSocketClientMessageReceived } from "../Message/WebSocketClientMessageReceived";
 
@@ -9,6 +9,11 @@ import { ProtocolBase } from "./ProtocolBase";
  */
 export class BasicProtocol extends ProtocolBase {
   /**
+   * Identificador  do protocolo.
+   */
+  private readonly identifier = "BASIC";
+
+  /**
    * Lista de mensagens para transmitir.
    */
   private readonly messages: string[] = [];
@@ -16,15 +21,28 @@ export class BasicProtocol extends ProtocolBase {
   /**
    * Recebe uma mensagem.
    */
-  public receive(message: string): void {
-    Logger.post(
-      "Websocket client received message: {0}",
-      message,
-      LogLevel.Verbose,
-      this.client.constructor.name
-    );
+  public receive(messageRaw: string): void {
+    try {
+      const message = this.extractMessage(messageRaw);
 
-    void new WebSocketClientMessageReceived(this.client, message).sendAsync();
+      Logger.post(
+        "Websocket client received message: {0}",
+        message,
+        LogLevel.Verbose,
+        this.client.constructor.name
+      );
+
+      void new WebSocketClientMessageReceived(this.client, message).sendAsync();
+    } catch (error) {
+      this.client.stop();
+
+      Logger.post(
+        "Websocket client stopped the connection because an error: {0}",
+        error instanceof Error ? error.message : String(error),
+        LogLevel.Warning,
+        this.client.constructor.name
+      );
+    }
   }
 
   /**
@@ -36,13 +54,46 @@ export class BasicProtocol extends ProtocolBase {
   }
 
   /**
+   * Codifica uma mensagem para envio.
+   * @param message Mensagem.
+   */
+  private encodeMessage(message: string): string {
+    return `${this.identifier}\n${Buffer.from(message).toString("base64")}`;
+  }
+
+  /**
+   * Valida se o protocolo é compatível.
+   * @param messageRaw Mensagem.
+   */
+  private extractMessage(messageRaw: string): string {
+    const lines = messageRaw.split("\n");
+
+    const firstLine = lines[0];
+    if (firstLine !== this.identifier) {
+      throw new InvalidDataError("Protocol not supported");
+    }
+
+    const message = lines[1];
+    if (message === undefined) {
+      throw new InvalidDataError("Blank message body");
+    }
+
+    try {
+      return Buffer.from(message, "base64").toString();
+    } catch (error) {
+      throw new InvalidDataError("Wrongly encoded message.");
+    }
+  }
+
+  /**
    * Obtém a última mensagem
    */
   private pooling(): void {
     const message = this.messages.shift();
 
     if (message !== undefined) {
-      this.client.sendRawMessage(message);
+      const messageRaw = this.encodeMessage(message);
+      this.client.sendRawMessage(messageRaw);
 
       Logger.post(
         "Websocket client sent a message: {0}",
