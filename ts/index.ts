e3502c6adde98f597afa670d657d497d8ea994de
Sergio Cabral <git@sergiocabral.com>
2022-01-20 23:17:57 -0300

    code: Ajuste no import de Main.

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,4 @@
-import { Main } from '@gohorse/npm-application/js/Application/Main';
+import { Main } from "@gohorse/npm-application";
 import { GenericError, Logger, LogLevel } from '@sergiocabral/helper';
 import { BusApplication } from './Executable/BusApplication';
 
