92ae37afb73e66c31da2474c98b288cfceac25e6
Sergio Cabral <git@sergiocabral.com>
2022-01-20 22:16:27 -0300

    code: Adicionado classe Main.

---
+++ ts/Application/Main.ts
@@ -0,0 +1,81 @@
+import { Argument } from "@gohorse/npm-core";
+import {
+  InvalidArgumentError,
+  InvalidExecutionError,
+  Logger,
+  LogLevel,
+} from "@sergiocabral/helper";
+import fs from "fs";
+import { Definition } from "../Definition";
+import { IApplication } from "./IApplication";
+
+/**
+ * Classe principal com o ponto de entrada da execução.
+ */
+export class Main {
+  /**
+   * Aplicação selecionada para execução.
+   */
+  public static async start(applicationConstructor: (new () => IApplication)): Promise<void> {
+    if (Main.instance !== undefined) {
+      throw new InvalidExecutionError(
+        "Main cannot be instantiated more than once."
+      );
+    }
+
+    Main.instance = new Main();
+    const application = new applicationConstructor();
+
+    const killFilename = `KILL.${Argument.getApplicationName()}`;
+    if (fs.existsSync(killFilename)) {
+      fs.unlinkSync(killFilename);
+    }
+
+    if (!Argument.hasStopArgument()) {
+      try {
+        await application.run();
+      } catch (error: unknown) {
+        await application.stop();
+        throw error;
+      }
+
+      const killVerifyIntervalTimer = setInterval(async () => {
+        if (fs.existsSync(killFilename)) {
+          clearInterval(killVerifyIntervalTimer);
+
+          fs.unlinkSync(killFilename);
+
+          Logger.post(
+            "Signal received to terminate application.",
+            undefined,
+            LogLevel.Information,
+            Main.name
+          );
+
+          await application.stop();
+        }
+      }, Definition.INTERVAL_BETWEEN_CHECKING_FLAG_FILE_IN_MILLISECONDS);
+
+      application.onStop.add(() => clearInterval(killVerifyIntervalTimer));
+    } else {
+      Logger.post(
+        "Sending signal to terminate application.",
+        undefined,
+        LogLevel.Information,
+        Main.name
+      );
+
+      fs.writeFileSync(killFilename, "");
+    }
+  }
+
+  /**
+   * Instância única da classe.
+   */
+  private static instance?: Main;
+
+  /**
+   * Construtor privado.
+   */
+  private constructor() {}
+}
--- ts/Definition.ts
+++ ts/Definition.ts
@@ -8,6 +8,11 @@ export class Definition {
   public static readonly DEFAULT_INTERVAL_BETWEEN_PING_TO_SERVER_IN_SECONDS = 60;
 
   /**
+   * Intervalo entre verificações para saber se é para finalizar a aplicação.
+   */
+  public static readonly INTERVAL_BETWEEN_CHECKING_FLAG_FILE_IN_MILLISECONDS = 5000;
+
+  /**
    * Constante de 1 segundo como milissegundos.
    */
   public static readonly ONE_SECOND_IN_MILLISECOND = 1000;
