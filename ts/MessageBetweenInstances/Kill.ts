9c81aaccdcfb3f04090d2f695f5a97b85b324d44
Sergio Cabral <git@sergiocabral.com>
2022-05-25 17:30:42 -0300

    comment: Removido comentário.

--- ts/MessageBetweenInstances/Kill.ts
+++ ts/MessageBetweenInstances/Kill.ts
@@ -8,31 +8,4 @@ export class Kill extends MessageToInstance {
    * Tipo da mensagem.
    */
   public override type = 'Kill';
-
-  // // TODO: Essa era a lógica original. Voltar ela pra algum lugar
-  //
-  // try {
-  //   fs.unlinkSync(this.instanceFile);
-  //
-  //   Logger.post(
-  //     'Terminated instance "{instanceId}" by deleting execution signal file: {instanceFile}',
-  //     {
-  //       instanceId: this.instanceId,
-  //       instanceFile: this.instanceFile
-  //     },
-  //     LogLevel.Information,
-  //     Kill.logContext2
-  //   );
-  // } catch (error) {
-  //   Logger.post(
-  //     'Error to terminate instance "{instanceId}" by deleting execution signal file: {instanceFile}. ERROR: {error}',
-  //     {
-  //       instanceId: this.instanceId,
-  //       instanceFile: this.instanceFile,
-  //       error
-  //     },
-  //     LogLevel.Error,
-  //     Kill.logContext2
-  //   );
-  // }
 }
