2d075d354f2c9438e0443f0547e4d00bff0564e8
Sergio Cabral <git@sergiocabral.com>
2022-05-28 16:31:19 -0300

    refactor: Ordenação dos imports em index.ts

--- ts/index.ts
+++ ts/index.ts
@@ -1,19 +1,20 @@
 #! /usr/bin/env node
 
-import { ConfigurationReloaded } from './BusMessage/Event/ConfigurationReloaded';
 import { ReloadConfiguration } from './BusMessage/Application/Message/ReloadConfiguration';
 import { TerminateApplication } from './BusMessage/Application/Message/TerminateApplication';
 
 import { IApplicationMessage } from './BusMessage/Application/IApplicationMessage';
 import { ApplicationMessage } from './BusMessage/Application/ApplicationMessage';
 
+import { ConfigurationReloaded } from './BusMessage/Event/ConfigurationReloaded';
+
 import { Generate } from './Helper/Generate';
 
 export {
-  ConfigurationReloaded,
   ReloadConfiguration,
   TerminateApplication,
   IApplicationMessage,
   ApplicationMessage,
+  ConfigurationReloaded,
   Generate
 };
