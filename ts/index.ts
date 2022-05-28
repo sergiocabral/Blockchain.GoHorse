895f9c28279dafaf447181b99178c743c00f8154
Sergio Cabral <git@sergiocabral.com>
2022-05-27 23:16:16 -0300

    comment: TODOs

--- ts/index.ts
+++ ts/index.ts
@@ -3,5 +3,7 @@
 import { SampleApp } from './Sample/SampleApp';
 
 // TODO: Implementar log para elasticsearch
+// TODO: Criar packages npm-log, npm-log-console, npm-log-file, npm-log-elasticsearch
+// TODO: Mover MessageBetweenInstances para npm-core
 
 void new SampleApp().run();
