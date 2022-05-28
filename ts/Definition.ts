2524b766c1b3846cf774fad338e88adf60399418
Sergio Cabral <git@sergiocabral.com>
2022-05-28 00:25:10 -0300

    comment: DEFAULT_LANGUAGE

--- ts/Definition.ts
+++ ts/Definition.ts
@@ -4,6 +4,8 @@
 export class Definition {
   /**
    * Idioma padrão.
+   * Será usado tanto para o idioma selecionado.
+   * Como também para o idioma padrão caso o idioma selecionado seja inválido.
    */
   public static readonly DEFAULT_LANGUAGE = 'en-US';
 }
