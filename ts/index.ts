e8cf0ccdc182f85d3a2aaaf7ad8ee207630dde80
Sergio Cabral <git@sergiocabral.com>
2022-05-30 20:41:19 -0300

    comment

--- ts/index.ts
+++ ts/index.ts
@@ -3,6 +3,5 @@
 import { SampleApp } from './Sample/SampleApp';
 
 // TODO: Implementar log para elasticsearch
-// TODO: Criar packages npm-log, npm-log-console, npm-log-file, npm-log-elasticsearch
 
 void new SampleApp().run();
