db2abbcaeaabb820e85ec22f6a5da90aeafe7288
Sergio Cabral <git@sergiocabral.com>
2022-05-30 19:06:55 -0300

    configure+code: Ajuste de npm-template para npm-log-file

--- ts/Dummy/Foo.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Uma classe qualquer.
- */
-export class Foo {
-  /**
-   * Outra coisa qualquer.
-   */
-  public bar: number = Math.random();
-}
--- ts/index.ts
+++ ts/index.ts
@@ -1,11 +1,2 @@
 #! /usr/bin/env node
 
-import { Foo } from './Dummy/Foo';
-
-export { Foo };
-
-/*
-This file cannot be compiled because it is a template.
-Read more in the "Creating a new NPM package" topic in the README.md file.
-To prevent and mistakenly publish in NPM the line below breaks the build.
-*/ cannot build
