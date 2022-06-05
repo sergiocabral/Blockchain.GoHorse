dc1e4d31349b7a7fab30b94f7001a96881d1d605
Sergio Cabral <git@sergiocabral.com>
2022-06-05 15:13:18 -0300

    code: Adicionado constantes para milesegundos

--- ts/GlobalDefinition.ts
+++ ts/GlobalDefinition.ts
@@ -3,6 +3,29 @@
  */
 export class GlobalDefinition {
   /**
+   * Tempo de 1 segundo em milesegundos.
+   */
+  public static readonly TIME_OF_ONE_SECOND_IN_MILLISECONDS = 1000;
+
+  /**
+   * Tempo de 1 minuto em milesegundos.
+   */
+  public static readonly TIME_OF_ONE_MINUTE_IN_MILLISECONDS =
+    GlobalDefinition.TIME_OF_ONE_SECOND_IN_MILLISECONDS * 60;
+
+  /**
+   * Tempo de 1 hora em milesegundos.
+   */
+  public static readonly TIME_OF_ONE_HOUR_IN_MILLISECONDS =
+    GlobalDefinition.TIME_OF_ONE_MINUTE_IN_MILLISECONDS * 60;
+
+  /**
+   * Tempo de 1 dia em milesegundos.
+   */
+  public static readonly TIME_OF_ONE_DAY_IN_MILLISECONDS =
+    GlobalDefinition.TIME_OF_ONE_HOUR_IN_MILLISECONDS * 24;
+
+  /**
    * Prefixo dos arquivos de ambiente.
    */
   public static readonly ENVIRONMENT_FILE_PREFIX = 'env';
