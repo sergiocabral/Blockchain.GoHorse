63ef6ab9e1037fa4564f540b075ac131065d7f4e
Sergio Cabral <git@sergiocabral.com>
2022-06-07 01:18:30 -0300

    code: minimumLevel como Verbose

--- ts/Console/LogToConsoleConfiguration.ts
+++ ts/Console/LogToConsoleConfiguration.ts
@@ -1,6 +1,12 @@
 import { LoggerToStreamConfiguration } from '@gohorse/npm-log';
+import { LogLevel } from '@sergiocabral/helper';
 
 /**
  * Configurações do logger.
  */
-export class LogToConsoleConfiguration extends LoggerToStreamConfiguration {}
+export class LogToConsoleConfiguration extends LoggerToStreamConfiguration {
+  /**
+   * Nível mínimo aceito para postar a mensagem do log.
+   */
+  public override minimumLevel = LogLevel[LogLevel.Verbose];
+}
