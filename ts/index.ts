362c5ef3112eca9634e76662bdfc9a7b076d24e7
Sergio Cabral <git@sergiocabral.com>
2022-06-06 23:49:20 -0300

    comment

--- ts/index.ts
+++ ts/index.ts
@@ -2,6 +2,7 @@
 
 import { SampleApp } from './Sample/SampleApp';
 
+// TODO: Revisar informação do log para remover dados privados do usuário, por exemplo o path dos arquivos de tradução.
 // TODO: Rever necessidade de Logger em toda aplicação e pacotes npm-*
 
 void new SampleApp().run();
