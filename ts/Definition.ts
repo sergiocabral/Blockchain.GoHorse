fef24470e488953d06bdbd95d58ce53dead068e3
Sergio Cabral <git@sergiocabral.com>
2022-06-09 14:21:42 -0300

    configure+code: Trazendo código de npm-application
    Momento do código no hash: db71900dc4b507b421a49c7b2de7dd1d503db94d

---
+++ ts/Definition.ts
@@ -0,0 +1,32 @@
+import { GlobalDefinition } from '@gohorse/npm-core';
+
+/**
+ * Definições hard-coded.
+ */
+export class Definition {
+  /**
+   * Serviço padrão do elasticsearch. Url.
+   */
+  public static readonly DEFAULT_ELASTICSEARCH_SERVER = `elasticsearch.${GlobalDefinition.INTERNET_DOMAIN_FOR_GOHORSE}`;
+
+  /**
+   * Serviço padrão do elasticsearch. Protocol.
+   */
+  public static readonly DEFAULT_ELASTICSEARCH_PROTOCOL = 'https';
+
+  /**
+   * Serviço padrão do elasticsearch. Porta.
+   */
+  public static readonly DEFAULT_ELASTICSEARCH_PORT = 443;
+
+  /**
+   * Serviço padrão do elasticsearch. Usuário.
+   */
+  public static readonly DEFAULT_ELASTICSEARCH_USER = `guest.${GlobalDefinition.INTERNET_DOMAIN_FOR_GOHORSE}`;
+
+  /**
+   * Serviço padrão do elasticsearch. Password.
+   */
+  public static readonly DEFAULT_ELASTICSEARCH_PASSWORD =
+    GlobalDefinition.WELL_KNOWN_PASSWORD;
+}
---
+++ ts/ElasticSearch/ElasticSearchDatabase.ts
@@ -0,0 +1,159 @@
+import { ElasticSearchDatabaseConfiguration } from './ElasticSearchDatabaseConfiguration';
+import { Client } from '@elastic/elasticsearch';
+import { ClientOptions } from '@elastic/elasticsearch/lib/client';
+import {
+  EmptyError,
+  HelperObject,
+  HttpStatusCode,
+  Logger,
+  LogLevel,
+  PrimitiveValueType
+} from '@sergiocabral/helper';
+import { Get, TemplateString } from '@gohorse/npm-core';
+import { ResponseError } from '@elastic/transport/lib/errors';
+import { Database, IDatabasePushOnly } from '@gohorse/npm-database';
+
+/**
+ * Conexão com o banco de dados ElasticSearch.
+ */
+export class ElasticSearchDatabase
+  extends Database<ElasticSearchDatabaseConfiguration>
+  implements IDatabasePushOnly
+{
+  /**
+   * Contenxto de log.
+   */
+  private static logContext = 'ElasticSearchDatabase';
+
+  /**
+   * Cliente do banco de dados.
+   */
+  private clientValue?: Client;
+
+  /**
+   * Cliente do banco de dados.
+   */
+  private get client(): Client {
+    if (this.clientValue === undefined) {
+      throw new EmptyError('Client was not created.');
+    }
+    return this.clientValue;
+  }
+
+  /**
+   * Sufixo adicionado ao final do index para dados recebidos via interface IDatabasePushOnly
+   */
+  public pushOnlyIndexSuffix = '';
+
+  /**
+   * Grava um conjuntos de valores em única via, tipo log.
+   * @param values Campos e valores.
+   * @param extra Valores extra
+   * @param indexSuffix Sufixo do índice no ElasticSearch
+   */
+  public async push(
+    values: Record<string, PrimitiveValueType | Date | undefined>,
+    extra?: unknown,
+    indexSuffix?: string
+  ): Promise<this> {
+    indexSuffix = indexSuffix ?? this.pushOnlyIndexSuffix;
+    const index = TemplateString.replace(
+      this.configuration.indexPrefixTemplate + indexSuffix
+    ).toLowerCase();
+
+    const separator = '__';
+
+    const body: Record<string, unknown> = {
+      ...HelperObject.flattenWithSafeType(values, separator),
+      field: HelperObject.flattenWithSafeType(extra, separator)
+    };
+
+    for (const key in body) {
+      if (key.endsWith(separator + 'date')) {
+        delete body[key];
+      }
+    }
+
+    await this.client.index({
+      index,
+      body
+    });
+
+    return this;
+  }
+
+  /**
+   * Força a finalização de qualquer conexão aberta e redefine a instância da conexão.
+   */
+  protected override async resetConnection(): Promise<void> {
+    await this.closeConnection();
+  }
+
+  /**
+   * Fecha a conexão
+   */
+  protected override async closeConnection(): Promise<void> {
+    if (this.clientValue !== undefined) {
+      await this.clientValue.close();
+      this.clientValue = undefined;
+    }
+  }
+
+  /**
+   * Abre a conexão
+   */
+  protected override async openConnection(
+    configuration: ElasticSearchDatabaseConfiguration
+  ): Promise<void> {
+    const options: ClientOptions = {
+      node: `${configuration.protocol}://${configuration.server}:${configuration.port}`
+    };
+    if (typeof configuration.username === 'string') {
+      options.auth = {
+        username: configuration.username,
+        password: Get.password(configuration.password ?? '')
+      };
+    }
+    Logger.post(
+      'Connecting ({authenticationMode}) to an ElasticSearch database at address: {url}',
+      {
+        url: options.node,
+        authenticationMode:
+          options.auth !== undefined
+            ? 'with username and password'
+            : 'without username and password'
+      },
+      LogLevel.Debug,
+      ElasticSearchDatabase.logContext
+    );
+    this.clientValue = new Client(options);
+    let httpResponseStatusCode: number;
+    try {
+      Logger.post(
+        'Sending a PING to make sure the connection has been established.',
+        undefined,
+        LogLevel.Verbose,
+        ElasticSearchDatabase.logContext
+      );
+      await this.clientValue.ping();
+      httpResponseStatusCode = 200;
+    } catch (error: unknown) {
+      const connected =
+        error instanceof ResponseError &&
+        error.statusCode === HttpStatusCode.Forbidden;
+      if (!connected) {
+        throw error;
+      }
+      httpResponseStatusCode = error.statusCode;
+    }
+    Logger.post(
+      'There was a satisfactory response from PING with the HTTP code {httpStatusCode} {httpStatusName}.',
+      {
+        httpStatusCode: httpResponseStatusCode,
+        httpStatusName: HttpStatusCode[httpResponseStatusCode]
+      },
+      LogLevel.Verbose,
+      ElasticSearchDatabase.logContext
+    );
+  }
+}
