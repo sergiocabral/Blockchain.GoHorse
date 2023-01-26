2b3ff76c1ad4df73bd6912b58102d8940bfe24bc
Sergio Cabral <sergio.cabral@sapiensia.com>
2023-01-26 13:09:59 -0300

    feat: GitWrapper com método version funcional
    - Antes retornava a saída de texto.
    - Agora somente o valor da versão.

--- ts/Git/GitWrapper.ts
+++ ts/Git/GitWrapper.ts
@@ -1,4 +1,5 @@
 import { ApplicationWrapper } from '../Wrapper/ApplicationWrapper';
+import { InvalidExecutionError } from '@sergiocabral/helper';
 
 /**
  * ProcessExecution para executar o Git.
@@ -14,6 +15,14 @@ export class GitWrapper extends ApplicationWrapper {
    */
   public async version(): Promise<string> {
     const output = await super.run('--version');
-    return output.all;
+    const regexExtractVersion = /\d[\w.-]+\w/;
+    const version = output.all.match(regexExtractVersion);
+    if (version?.length !== 1) {
+      throw new InvalidExecutionError(
+        'Error when execute git. Full output: ' + output.all
+      );
+    }
+
+    return version[0];
   }
 }
