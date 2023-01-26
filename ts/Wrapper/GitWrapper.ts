2397b86fcf088a38d471dcd01e26969fc4a8969e
Sergio Cabral <sergio.cabral@sapiensia.com>
2023-01-26 12:04:38 -0300

    feat: implementação inicial do wrapper de git
    - Apenas demonstrado que funciona

---
+++ ts/Wrapper/GitWrapper.ts
@@ -0,0 +1,16 @@
+import { ApplicationWrapper } from './ApplicationWrapper';
+import { ProcessExecution } from '../CommandLine/ProcessExecution';
+
+/**
+ * CommandLine para executar o Git.
+ */
+export class GitWrapper extends ApplicationWrapper {
+  /**
+   * Executa a aplicação.
+   */
+  public async run(): Promise<void> {
+    const processExecution = new ProcessExecution('git', ['--version'], 'C:\\');
+    const result = await processExecution.execute();
+    console.log(result.all);
+  }
+}
