57561595ce01a39d2c51964f7599bc42408bcbc2
Sergio Cabral <sergio.cabral@sapiensia.com>
2023-01-26 13:11:52 -0300

    feat: GitWrapper com método version

--- ts/Git/GitWrapper.ts
+++ ts/Git/GitWrapper.ts
@@ -8,4 +8,12 @@ export class GitWrapper extends ApplicationWrapper {
    * Caminho da aplicação.
    */
   public override readonly path = 'git';
+
+  /**
+   * Obter versão do Git.
+   */
+  public async version(): Promise<string> {
+    const output = await super.run('--version');
+    return output.all;
+  }
 }
