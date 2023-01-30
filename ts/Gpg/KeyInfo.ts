76828797a378f709e837f95c20c17cd18936b6b9
Sergio Cabral <sergio.cabral@sapiensia.com>
2023-01-30 14:05:29 -0300

    feat: classe KeyInfo extraindo campo expires
    - Implementação inicial apenas
    - Expires extraido como Date
    - Mas ainda exibe timezone

--- ts/Gpg/KeyInfo.ts
+++ ts/Gpg/KeyInfo.ts
@@ -1,5 +1,3 @@
-import { IProcessExecutionOutput } from '../ProcessExecution/IProcessExecutionOutput';
-
 /**
  * Informaçõesa sobre uma chave GPG
  */
@@ -8,13 +6,38 @@ export class KeyInfo {
    * Faz um parse da saída do comando `gpg --list-keys`
    * @param output Output bruto do comando gpg
    */
-  public static parse(output: IProcessExecutionOutput): KeyInfo[] {
-    return [];
+  public static parse(output: string): KeyInfo[] {
+    const result: KeyInfo[] = [];
+
+    const regexExtractExpires =
+      /(?<=expires: )(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/;
+
+    const blocks = output.split('\n');
+    for (const block of blocks) {
+      const keyInfo = new KeyInfo();
+
+      const expires = regexExtractExpires.exec(block);
+      if (
+        expires !== null &&
+        expires.length > 0 &&
+        expires.groups !== undefined
+      ) {
+        keyInfo.expires = new Date(
+          Number(expires.groups['year']),
+          Number(expires.groups['month']) - 1,
+          Number(expires.groups['day'])
+        );
+        // TODO: Escrever na lib @sergiocabral/helper algo para remover o timezone.
+      }
+
+      result.push(keyInfo);
+    }
+
+    return result;
   }
 
   /**
-   * Construtor.
-   * @param output Saída do comando GPG para uma determinada chave.
+   * Data de expiração.
    */
-  public constructor(public readonly output?: string) {}
+  public expires?: Date;
 }
