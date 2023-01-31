4e07f9acf81977d0023ce52eac088cc72d82f1c9
Sergio Cabral <sergio.cabral@sapiensia.com>
2023-01-31 12:58:50 -0300

    refactor: movido lógica para função à parte
    - simplificado função `parse` para chamar individualmente os parses de cada campo.

--- ts/Gpg/KeyInfo.ts
+++ ts/Gpg/KeyInfo.ts
@@ -3,40 +3,53 @@
  */
 export class KeyInfo {
   /**
+   * Extrai o campo da saída do GPG: expires.
+   */
+  private static extractFieldExpires(output: string): Date | undefined {
+    const regexExtractExpires =
+      /(?<=expires: )(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/;
+
+    const timezoneOffset = new Date().getTimezoneOffset();
+
+    const expires = regexExtractExpires.exec(output);
+    if (
+      expires !== null &&
+      expires.length > 0 &&
+      expires.groups !== undefined
+    ) {
+      return new Date(
+        Number(expires.groups['year']),
+        Number(expires.groups['month']) - 1,
+        Number(expires.groups['day'])
+      ).addMinutes(-timezoneOffset);
+    }
+
+    return undefined;
+  }
+
+  /**
    * Faz um parse da saída do comando `gpg --list-keys`
    * @param output Output bruto do comando gpg
    */
   public static parse(output: string): KeyInfo[] {
     const result: KeyInfo[] = [];
 
-    const timezoneOffset = new Date().getTimezoneOffset();
-
-    const regexExtractExpires =
-      /(?<=expires: )(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/;
-
     const blocks = output.split('\n');
     for (const block of blocks) {
       const keyInfo = new KeyInfo();
 
-      const expires = regexExtractExpires.exec(block);
-      if (
-        expires !== null &&
-        expires.length > 0 &&
-        expires.groups !== undefined
-      ) {
-        keyInfo.expires = new Date(
-          Number(expires.groups['year']),
-          Number(expires.groups['month']) - 1,
-          Number(expires.groups['day'])
-        ).addMinutes(-timezoneOffset);
-
-      }
+      keyInfo.expires = KeyInfo.extractFieldExpires(block);
 
       result.push(keyInfo);
     }
 
     return result;
   }
+
+  /**
+   * Algoritmo.
+   */
+  public algorithm?: string;
 
   /**
    * Data de expiração.
