6e351ee8a53acfbc608f1d9a873222c927f3292f
Sergio Cabral <sergio.cabral@sapiensia.com>
2023-01-31 13:09:03 -0300

    feat: KeyInfo extraindo data de emissão

--- ts/Gpg/KeyInfo.ts
+++ ts/Gpg/KeyInfo.ts
@@ -14,6 +14,27 @@ export class KeyInfo {
   /**
    * Extrai o campo da saída do GPG: expires.
    */
+  private static extractFieldIssued(output: string): Date | undefined {
+    const regexToExtract =
+      /(?<=pub\s+\w+\s+)(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/;
+
+    const timezoneOffset = new Date().getTimezoneOffset();
+
+    const value = regexToExtract.exec(output);
+    if (value !== null && value.length > 0 && value.groups !== undefined) {
+      return new Date(
+        Number(value.groups['year']),
+        Number(value.groups['month']) - 1,
+        Number(value.groups['day'])
+      ).addMinutes(-timezoneOffset);
+    }
+
+    return undefined;
+  }
+
+  /**
+   * Extrai o campo da saída do GPG: expires.
+   */
   private static extractFieldExpires(output: string): Date | undefined {
     const regexToExtract =
       /(?<=expires: )(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/;
@@ -44,6 +65,7 @@ export class KeyInfo {
       const keyInfo = new KeyInfo();
 
       keyInfo.algorithm = KeyInfo.extractFieldAlgorithm(block);
+      keyInfo.issued = KeyInfo.extractFieldIssued(block);
       keyInfo.expires = KeyInfo.extractFieldExpires(block);
 
       result.push(keyInfo);
@@ -56,6 +78,11 @@ export class KeyInfo {
    * Algoritmo.
    */
   public algorithm?: string;
+
+  /**
+   * Data de emissão.
+   */
+  public issued?: Date;
 
   /**
    * Data de expiração.
