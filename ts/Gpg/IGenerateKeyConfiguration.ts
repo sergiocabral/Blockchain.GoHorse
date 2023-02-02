9141813c4ed83ae15482acbe9de9df972fef862c
Sergio Cabral <git@sergiocabral.com>
2023-02-02 13:32:45 -0300

    feat: parametros de entrada para gerar uma chave gpg

---
+++ ts/Gpg/IGenerateKeyConfiguration.ts
@@ -0,0 +1,41 @@
+import { GpgKeyType } from './GpgKeyType';
+
+/**
+ * Parâmetrode entrada para gerar uma chave GPG
+ */
+export interface IGenerateKeyConfiguration {
+  /**
+   * Algoritmo da chave principal.
+   */
+  mainKeyType: GpgKeyType;
+
+  /**
+   * Tamanho da chave principal.
+   */
+  mainKeyLength: number;
+
+  /**
+   * Algoritmo da sub chave.
+   */
+  subKeyType: GpgKeyType;
+
+  /**
+   * Tamanho da sub chave.
+   */
+  subKeyLength: number;
+
+  /**
+   * Nome da pessoa.
+   */
+  nameReal: string;
+
+  /**
+   * Endereço do email
+   */
+  nameEmail: string;
+
+  /**
+   * Data de expiração.
+   */
+  expires: Date;
+}
