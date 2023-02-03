39dd4fd0fe85657b75290ce6f618268832ffa6f7
Sergio Cabral <sergio.cabral@sapiensia.com>
2023-02-03 15:49:54 -0300

    feat!: ajuste na lista de algoritmos
    - Lista sem erros, porém, removido o que era duvidoso. Trazer correto da documentação.
    - Usando chave e valor como string.

--- ts/Gpg/GpgKeyType.ts
+++ ts/Gpg/GpgKeyType.ts
@@ -3,8 +3,6 @@
  * https://lists.gnupg.org/pipermail/gnupg-devel/2017-April/032762.html
  */
 export enum GpgKeyType {
-  Rsa = 1,
-  DsaAndElgamal = 2,
-  DsaSignOnly = 3,
-  RsaSignOnly = 4
+  Default = 'default',
+  Rsa = 'RSA'
 }
